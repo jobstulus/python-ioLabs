dcf71682d175ee032698da42138e763ae45b9aa1
Stephan Jobstmann [s.jobstmann@sensorik-bayern.de] <s.jobstmann@sensorik-01.zz>
2018-06-20 17:39:13 +0200

    Changes to work with python3.x

--- ioLabs.py
+++ ioLabs.py
@@ -12,7 +12,8 @@ import logging
 
 import time
 import struct
-from Queue import Queue, Empty
+import queue as Queue 
+from queue import Empty
 import hid
 
 IO_LABS_VENDOR_ID=0x19BC
@@ -976,14 +977,14 @@ if __name__ == '__main__':
     
     usbbox=USBBox()
     
-    print "USBBox connected"
-    print "serial #:",usbbox.serial_num
-    print "version:",usbbox.version
-    print "voice version:", usbbox.voice_version
+    print ("USBBox connected")
+    print ("serial #:",usbbox.serial_num)
+    print ("version:",usbbox.version)
+    print ("voice version:", usbbox.voice_version)
     
     # attached a callback for every report type
     def report_callback(msg):
-        print "received:",msg
+        print ("received:",msg)
     for command_id in REPORT.ALL_IDS():
         usbbox.commands.add_callback(command_id,report_callback)
         
@@ -1004,14 +1005,14 @@ if __name__ == '__main__':
             continue
         elif command == 'help':
             # print list of available commands
-            print "commands:"
-            print " exit"
-            print " help"
+            print ("commands:")
+            print (" exit")
+            print (" help")
             for command_id in COMMAND_SUMMARY.keys():
                 command_name=COMMAND_SUMMARY[command_id][0].lower()
                 command_args=COMMAND_SUMMARY[command_id][2]
                 command_args=['<%s>' % arg for arg in command_args]
-                print " %s %s" % (command_name,' '.join(command_args))
+                print (" %s %s" % (command_name,' '.join(command_args)))
         else:
             try:
                 command_parts=command.split()
@@ -1023,18 +1024,18 @@ if __name__ == '__main__':
                         known=True
                         break
                 if not known:
-                    print "error, unknown command: " + command_name
+                    print ("error, unknown command: " + command_name)
                 else:
                     command_fn=getattr(usbbox.commands,command_name)
                     # turn all arguments into int's
                     command_args=[int(arg) for arg in command_args]
                     command_fn(*command_args)
             except:
-                print "error running: " + command
+                print ("error running: " + command)
     
     # make sure we process any remaining reports
     usbbox.process_received_reports()
     usbbox.stop_recording()
     
-    print "recorded reports:"
-    print outfile.getvalue()
+    print ("recorded reports:")
+    print (outfile.getvalue())
